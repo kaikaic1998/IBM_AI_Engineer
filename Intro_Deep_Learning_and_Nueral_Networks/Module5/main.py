21dcbe85140e2340dba1791c7e2d4b0f00ce104c
kainianc1998 <kainianc@gmail.com>
2023-05-21 17:08:35 +0900

    modify

--- Intro_Deep_Learning_and_Nueral_Networks/Module5/main.py
+++ Intro_Deep_Learning_and_Nueral_Networks/Module5/main.py
@@ -1,17 +1,43 @@
 import torch
-from torch.utils.data import Dataset
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
 
 import pandas as pd
 
-# class concrete_data (Dataset):
-#     
+Class 
 
-#     def __len__(self):
-#         return len(self.)
+class concrete_data (Dataset):
+    def __init__(self, file_name):
+        file_data = pd.read_csv(file_name)
+
+        n_row = file_data.shape[0]
+        n_column = file_data.shape[1]
+
+        x = file_data.iloc[0:n_row, 0:n_column-1].values
+        y = file_data.iloc[0:n_row, n_column-1].values
+
+        self.x = torch.tensor(x)
+        self.y = torch.tensor(y)
+
+    def __len__(self):
+        return len(self.y)
     
-#     def __getitem__(self, idx):
-#         return self.
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
 
-file_data = pd.read_csv('concrete_data.csv')
-print(file_data.shape[0])
-print(file_data.shape[1])
+# Get dataset
+orig_dataset = concrete_data('concrete_data.csv')
+
+# Split image data randomly into train and validation set
+size_train = int(0.9 * len(orig_dataset))
+size_validation = len(orig_dataset) - size_train
+train_dataset, validation_dataset = random_split(orig_dataset, [size_train, size_validation])
+
+# Load datasets into DataLoader
+train_loader = DataLoader(dataset=train_dataset, batch_size=1)
+validation_loader = DataLoader(dataset=validation_dataset, batch_size=1)
+
+# Loss function
+criterion = nn.MSELoss()
+
+optimizer = torch.optim.Adam()
